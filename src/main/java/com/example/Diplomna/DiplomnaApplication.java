7e62e74764fb670f1f99eac722cfdea7fb20f571
Marina <marisham1223@gmail.com>
2023-11-03 22:51:08 +0200

    Initial commit

---
+++ src/main/java/com/example/Diplomna/DiplomnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Diplomna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomnaApplication.class, args);
+	}
+
+}
