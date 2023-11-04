b82d5489afde4d005dc2dbf86e7d55858c753893
Iryna1122 <Oliinykirina1122@gmail.com>
2023-11-04 21:44:28 +0200

    added models

---
+++ src/main/java/com/example/Diplomna/model/HashTagVideo.java
@@ -0,0 +1,15 @@
+package com.example.Diplomna.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+public class HashTagVideo implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false,updatable = false)
+    private Long id;
+    private Long hashtagId;
+    private Long videoId;
+}
---
+++ src/main/java/com/example/Diplomna/model/UnsubscribeNotification.java
@@ -0,0 +1,13 @@
+package com.example.Diplomna.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+@Entity
+public class UnsubscribeNotification implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false,updatable = false)
+    private Long id;
+    private Long unsubscribeId;
+}
