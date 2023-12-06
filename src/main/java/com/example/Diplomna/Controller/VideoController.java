97428579e207117cdb0556f02094031ca3cfa068
Marina <marisham1223@gmail.com>
2023-12-06 21:59:55 +0200

    adsazf

---
+++ src/main/java/com/example/Diplomna/Controller/VideoController.java
@@ -0,0 +1,26 @@
+package com.example.Diplomna.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/video")
+public class VideoController {
+    @GetMapping("/add")
+    public String addCategory(Model model)
+    {
+        return "rdfigjrdifjhidh";
+    }
+
+    @GetMapping("/info")
+    public String videoData(Principal principal) //інжектимо поточного користувача через принципал
+    {
+        return principal.getName();
+    }
+
+}
