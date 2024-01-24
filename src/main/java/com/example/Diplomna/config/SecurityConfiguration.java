3a3b5ff519a573578674c51fb2d26de6b12d1681
YehorBelenkov <yehorbelenkov@gmail.com>
2024-01-24 14:11:37 -0600

    Blah blah blah blah fixed error Yehor Works Ok!

--- src/main/java/com/example/Diplomna/config/SecurityConfiguration.java
+++ src/main/java/com/example/Diplomna/config/SecurityConfiguration.java
@@ -29,6 +29,7 @@ public class SecurityConfiguration {
                 .permitAll()
                 .anyRequest()
                 .authenticated()
+//                .permitAll()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
